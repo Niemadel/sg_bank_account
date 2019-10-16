00f474ff5aad4a02b88b1dde66cc7e2a44d4abfb
Damien <damien.lequeux@gmail.com>
2019-10-16 12:11:40 +0200

    rename test and add test with negative amount

--- src/test/java/com/bankaccountkata/AccountTest.java
+++ src/test/java/com/bankaccountkata/AccountTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-    void deposit_of_100_on_account_save_100_on_account() {
+    void deposit_of_100_on_account_return_balance_of_100_on_account_with_balance_of_0() {
         Amount expectedBalance = Amount.of(new BigDecimal(100));
         Amount accountBalance = Amount.of(new BigDecimal(0));
         Account account = new Account(accountBalance);
@@ -21,9 +21,33 @@ public class AccountTest {
     }
 
     @Test
-    void withdrawal_of_100_on_account_substract_100_on_account() {
+    void deposit_of_100_on_account_return_balance_of_0_on_account_with_balance_of_minus_100() {
+        Amount expectedBalance = Amount.of(new BigDecimal(0));
+        Amount accountBalance = Amount.of(new BigDecimal(-100));
+        Account account = new Account(accountBalance);
+        Amount depositAmount = Amount.of(new BigDecimal(100));
+
+        account.deposit(depositAmount);
+
+        assertThat(account.balance).isEqualTo(expectedBalance);
+    }
+
+    @Test
+    void withdrawal_of_100_on_account_substract_100_on_account_with_initial_balance_of_200() {
         Amount expectedBalance = Amount.of(new BigDecimal(100));
         Amount accountBalance = Amount.of(new BigDecimal(200));
+        Account account = new Account(accountBalance);
+        Amount withdrawalAmount = Amount.of(new BigDecimal(100));
+
+        account.withdrawal(withdrawalAmount);
+
+        assertThat(account.balance).isEqualTo(expectedBalance);
+    }
+
+    @Test
+    void withdrawal_of_100_on_account_substract_100_on_account_with_balance_of_0() {
+        Amount expectedBalance = Amount.of(new BigDecimal(-100));
+        Amount accountBalance = Amount.of(new BigDecimal(0));
         Account account = new Account(accountBalance);
         Amount withdrawalAmount = Amount.of(new BigDecimal(100));
 
